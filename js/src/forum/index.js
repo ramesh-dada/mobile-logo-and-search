535e71bac85ba6e8a24036c1c2ce8297ddc5bf7c
RameshDada <75124237+ramesh-dada@users.noreply.github.com>
2021-07-12 14:32:03 +0530

    Update index.js

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -3,27 +3,14 @@ import app from 'flarum/forum/app';
 import Button from 'flarum/common/components/Button';
 import Search from 'flarum/components/Search';
 
-/* global m */
+/* RameshDADA Ultra PREMIUM */
 
 app.initializers.add('ramesh-dada-mobile-flarum-newdiscuss', () => {
 extend(HeaderPrimary.prototype, 'view', (component) => {
-            const canStartDiscussion = app.forum.attribute('canStartDiscussion') || !app.session.user;
-    items.add(
-      'newDiscussion',
-      Button.component(
-        {
-          icon: 'fas fa-edit',
-          className: 'Button Button--primary IndexPage-newDiscussion',
-          itemClassName: 'DadaDiscuss',
-          onclick: () => {
-            // If the user is not logged in, the promise rejects, and a login modal shows up.
-            // Since that's already handled, we dont need to show an error message in the console.
-            return this.newDiscussionAction().catch(() => {});
-          },
-          disabled: !canStartDiscussion,
-        },
-        app.translator.trans(canStartDiscussion ? 'core.forum.index.start_discussion_button' : 'core.forum.index.cannot_start_discussion_button')
-      )
-    );
+items.add('mobilelogo', <a onclick: () => {document.querySelector('#home-link').click();}><img src={app.forum.attribute('logoUrl')} alt={app.forum.attribute('forum_title')} /></a>, -100);
+items.add('search', Search.component({
+            state: app.search,
+        }), -100);
+
     });
 });
