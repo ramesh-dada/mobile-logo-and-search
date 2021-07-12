689006e4a5b1d0357d83bffab3db9c1db996a9f8
RameshDada <iusewgogoi@gmail.com>
2021-07-12 14:55:47 +0530

    Update index.js

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,16 +1,19 @@
 import {extend} from 'flarum/common/extend';
 import app from 'flarum/forum/app';
-import Button from 'flarum/common/components/Button';
+
 import Search from 'flarum/components/Search';
 
 /* RameshDADA Ultra PREMIUM */
 
-app.initializers.add('ramesh-dada-mobile-flarum-newdiscuss', () => {
+app.initializers.add('ramesh-dada-mobile-logo', () => {
 extend(HeaderPrimary.prototype, 'view', (component) => {
-items.add('mobilelogo', <a onclick: () => {document.querySelector('#home-link').click();}><img src={app.forum.attribute('logoUrl')} alt={app.forum.attribute('forum_title')} /></a>, -100);
+    items.add('mobilelogo', <a onclick={home()}><img src={app.forum.attribute('logoUrl')} alt={app.forum.attribute('forum_title')} /></a>, -100);
 items.add('search', Search.component({
             state: app.search,
         }), -100);
 
     });
+    function home() {
+        document.querySelector('#home-link').click();
+    }
 });
